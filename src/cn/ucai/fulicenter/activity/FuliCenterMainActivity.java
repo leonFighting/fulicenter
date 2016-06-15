4972d4690b577cddba8caeefd498df5dc40f1e78
leonFighting <379029170@qq.com>
2016-06-15 15:46:41 +0800

    初始化福利社activity,设置点击监听事件，点击Button高亮

--- src/cn/ucai/fulicenter/activity/FuliCenterMainActivity.java
+++ src/cn/ucai/fulicenter/activity/FuliCenterMainActivity.java
@@ -2,14 +2,77 @@ package cn.ucai.fulicenter.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import cn.ucai.fulicenter.R;
 
 public class FuliCenterMainActivity extends Activity {
-
+    TextView mtvCartHint;
+    RadioButton mRadioNewGood;
+    RadioButton mRadioBoutique;
+    RadioButton mRadioCategory;
+    RadioButton mRadioCart;
+    RadioButton mRadioPersonalCenter;
+    RadioButton[] mRadios = new RadioButton[5];
+    private int index;
+    //当前fragment的index
+    private int currentTabIndex;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fuli_center_main);
+        initView();
+    }
+
+    private void initView() {
+        mtvCartHint = (TextView) findViewById(R.id.tv_CartHint);
+
+        mRadioNewGood = (RadioButton) findViewById(R.id.layout_new_good);
+        mRadioBoutique = (RadioButton) findViewById(R.id.layout_boutique);
+        mRadioCategory = (RadioButton) findViewById(R.id.layout_category);
+        mRadioCart = (RadioButton) findViewById(R.id.layout_cart);
+        mRadioPersonalCenter = (RadioButton) findViewById(R.id.layout_personal_center);
+
+        mRadios[0] = mRadioNewGood;
+        mRadios[1] = mRadioBoutique;
+        mRadios[2] = mRadioCategory;
+        mRadios[3] = mRadioCart;
+        mRadios[4] = mRadioPersonalCenter;
+    }
+
+    public void onCheckedChange(View view) {
+        switch (view.getId()) {
+            case R.id.layout_new_good:
+                index = 0;
+                break;
+            case R.id.layout_boutique:
+                index = 1;
+                break;
+            case R.id.layout_category:
+                index = 2;
+                break;
+            case R.id.layout_cart:
+                index = 3;
+                break;
+            case R.id.layout_personal_center:
+                index = 4;
+                break;
+        }
+        if (currentTabIndex != index) {
+            setRadioChecked(index);
+            currentTabIndex = index;
+        }
+    }
+
+    private void setRadioChecked(int index) {
+        for (int i = 0; i < mRadios.length; i++) {
+            if (i == index) {
+                mRadios[i].setChecked(true);
+            } else {
+                mRadios[i].setChecked(false);
+            }
+        }
     }
 }
