3d20a22792d199434606a6804354d9ff2e2f6119
leonFighting <379029170@qq.com>
2016-06-18 10:42:46 +0800

    新增分类二级界面布局

--- src/cn/ucai/fulicenter/activity/CategoryDetailActivity.java
+++ src/cn/ucai/fulicenter/activity/CategoryDetailActivity.java
@@ -1,16 +1,13 @@
-package cn.ucai.fulicenter.fragment;
+package cn.ucai.fulicenter.activity;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.android.volley.Response;
@@ -19,49 +16,48 @@ import java.util.ArrayList;
 
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
-import cn.ucai.fulicenter.activity.FuliCenterMainActivity;
 import cn.ucai.fulicenter.adapter.GoodAdapter;
+import cn.ucai.fulicenter.bean.CategoryChildBean;
 import cn.ucai.fulicenter.bean.NewGoodBean;
 import cn.ucai.fulicenter.data.ApiParams;
 import cn.ucai.fulicenter.data.GsonRequest;
 import cn.ucai.fulicenter.utils.Utils;
+import cn.ucai.fulicenter.view.CatChildFilterButton;
+import cn.ucai.fulicenter.view.ColorFilterButton;
+import cn.ucai.fulicenter.view.DisplayUtils;
 
 /**
- * Created by leon on 16/6/17.
+ * Created by clawpo on 16/4/19.
  */
-public class NewGoodFragment extends Fragment {
-    //下载数据
-    public static int ACTION_DOWNLOAD = 0;
-    //上拉刷新
-    public static int ACTION_PULL_UP = 1;
-    //下拉刷新
-    public static int ACTION_PULL_DOWN = 2;
-    public static final String TAG = NewGoodFragment.class.getName();
-
-    FuliCenterMainActivity mContext;
+public class CategoryDetailActivity extends BaseActivity{
+    public static final String TAG = CategoryDetailActivity.class.getName();
     ArrayList<NewGoodBean> mGoodList;
     GoodAdapter mAdapter;
-    int pageId = 0;
-    int action = ACTION_DOWNLOAD;
+    private  int pageId = 0;
+    private int catId;
+    private int action = I.ACTION_DOWNLOAD;
     String path;
-
-    /** 下拉刷新控件*/
     SwipeRefreshLayout mSwipeRefreshLayout;
     RecyclerView mRecyclerView;
     TextView mtvHint;
     GridLayoutManager mGridLayoutManager;
+    Button mbtnPriceSort;
+    Button mbtnAddTimeSort;
 
-    @Nullable
+    CatChildFilterButton mbtnCatFilter;
+    ArrayList<CategoryChildBean> mChildList;
+    ColorFilterButton mbtnColorFilter;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        mContext = (FuliCenterMainActivity) getActivity();
-        View layout = View.inflate(mContext, R.layout.fragment_new_good,null);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_detail);
         mGoodList = new ArrayList<NewGoodBean>();
-        initView(layout);
+        initView();
         initData();
         setListener();
-        return layout;
     }
+
     private void setListener() {
         setPullDownRefreshListener();
         setPullUpRefreshListener();
@@ -77,17 +73,16 @@ public class NewGoodFragment extends Fragment {
                     @Override
                     public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
                         super.onScrollStateChanged(recyclerView, newState);
-                        Log.e(TAG,"setPullUpRefreshListener,newState="+newState);
                         if(newState == RecyclerView.SCROLL_STATE_IDLE &&
                                 lastItemPosition == mAdapter.getItemCount()-1){
                             if(mAdapter.isMore()){
                                 mSwipeRefreshLayout.setRefreshing(true);
-                                action = ACTION_PULL_UP;
-                                pageId += I.PAGE_SIZE_DEFAULT;
+                                action = I.ACTION_PULL_UP;
+                                pageId +=I.PAGE_SIZE_DEFAULT;
                                 getPath(pageId);
-                                mContext.executeRequest(new GsonRequest<NewGoodBean[]>(path,
+                                executeRequest(new GsonRequest<NewGoodBean[]>(path,
                                         NewGoodBean[].class,responseDownloadNewGoodListener(),
-                                        mContext.errorListener()));
+                                        errorListener()));
                             }
                         }
                     }
@@ -95,7 +90,9 @@ public class NewGoodFragment extends Fragment {
                     @Override
                     public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
                         super.onScrolled(recyclerView, dx, dy);
+                        //获取最后列表项的下标
                         lastItemPosition = mGridLayoutManager.findLastVisibleItemPosition();
+                        //解决RecyclerView和SwipeRefreshLayout共用存在的bug
                         mSwipeRefreshLayout.setEnabled(mGridLayoutManager
                                 .findFirstCompletelyVisibleItemPosition() == 0);
                     }
@@ -113,30 +110,31 @@ public class NewGoodFragment extends Fragment {
                     public void onRefresh() {
                         mtvHint.setVisibility(View.VISIBLE);
                         pageId = 0;
-                        action = ACTION_PULL_DOWN;
+                        action = I.ACTION_PULL_DOWN;
                         getPath(pageId);
-                        mContext.executeRequest(new GsonRequest<NewGoodBean[]>(path,
+                        executeRequest(new GsonRequest<NewGoodBean[]>(path,
                                 NewGoodBean[].class,responseDownloadNewGoodListener(),
-                                mContext.errorListener()));
+                                errorListener()));
                     }
                 }
         );
     }
-
     private void initData() {
         try {
+            catId = getIntent().getIntExtra(I.CategoryChild.CAT_ID,0);
             getPath(pageId);
-            mContext.executeRequest(new GsonRequest<NewGoodBean[]>(path,
+            executeRequest(new GsonRequest<NewGoodBean[]>(path,
                     NewGoodBean[].class,responseDownloadNewGoodListener(),
-                    mContext.errorListener()));
+                    errorListener()));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
     private String getPath(int pageId){
         try {
             path = new ApiParams()
-                    .with(I.NewAndBoutiqueGood.CAT_ID, I.CAT_ID+"")
+                    .with(I.NewAndBoutiqueGood.CAT_ID, catId+"")
                     .with(I.PAGE_ID, pageId+"")
                     .with(I.PAGE_SIZE, I.PAGE_SIZE_DEFAULT+"")
                     .getRequestUrl(I.REQUEST_FIND_NEW_BOUTIQUE_GOODS);
@@ -153,18 +151,18 @@ public class NewGoodFragment extends Fragment {
             @Override
             public void onResponse(NewGoodBean[] newGoodBeen) {
                 if(newGoodBeen!=null) {
-                    Log.e(TAG, "onResponse,action="+action);
                     mAdapter.setMore(true);
                     mSwipeRefreshLayout.setRefreshing(false);
                     mtvHint.setVisibility(View.GONE);
                     mAdapter.setFooterText(getResources().getString(R.string.load_more));
+                    //将数组转换为集合
                     ArrayList<NewGoodBean> list = Utils.array2List(newGoodBeen);
-                    if (action == ACTION_DOWNLOAD || action == ACTION_PULL_DOWN) {
+                    if (action == I.ACTION_DOWNLOAD || action == I.ACTION_PULL_DOWN) {
                         mAdapter.initList(list);
-                    } else if (action == ACTION_PULL_UP) {
+                    } else if (action == I.ACTION_PULL_UP) {
                         mAdapter.addItem(list);
                     }
-                    if(newGoodBeen.length< I.PAGE_SIZE_DEFAULT){
+                    if(newGoodBeen.length<I.PAGE_SIZE_DEFAULT){
                         mAdapter.setMore(false);
                         mAdapter.setFooterText(getResources().getString(R.string.no_more));
                     }
@@ -173,21 +171,27 @@ public class NewGoodFragment extends Fragment {
         };
     }
 
-    private void initView(View layout) {
-        mSwipeRefreshLayout = (SwipeRefreshLayout) layout.findViewById(R.id.srl_new_good);
+    private void initView() {
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.srl_category);
         mSwipeRefreshLayout.setColorSchemeColors(
                 R.color.google_blue,
                 R.color.google_green,
                 R.color.google_red,
                 R.color.google_yellow
         );
-        mtvHint = (TextView) layout.findViewById(R.id.tv_refresh_hint);
-        mGridLayoutManager = new GridLayoutManager(mContext, I.COLUM_NUM);
+        mtvHint = (TextView) findViewById(R.id.tv_refresh_hint);
+        mGridLayoutManager = new GridLayoutManager(this, I.COLUM_NUM);
         mGridLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        mRecyclerView = (RecyclerView) layout.findViewById(R.id.rv_new_good);
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_category);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(mGridLayoutManager);
-        mAdapter = new GoodAdapter(mContext,mGoodList,I.SORT_BY_ADDTIME_DESC);
+        mAdapter = new GoodAdapter(this,mGoodList,I.SORT_BY_ADDTIME_DESC);
         mRecyclerView.setAdapter(mAdapter);
+        DisplayUtils.initBack(this);
+        mbtnPriceSort = (Button) findViewById(R.id.btn_price);
+        mbtnAddTimeSort = (Button) findViewById(R.id.btn_addTime);
+        mbtnCatFilter = (CatChildFilterButton) findViewById(R.id.btnCatChildFilter);
+        mbtnColorFilter= (ColorFilterButton) findViewById(R.id.btnColorFilter);
     }
+
 }
