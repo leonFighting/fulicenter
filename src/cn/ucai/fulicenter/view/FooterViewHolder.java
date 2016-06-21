b059c3a9ee83c69e215553eb4c2c2d72aa818256
leonFighting <379029170@qq.com>
2016-06-21 13:09:24 +0800

    佛祖保佑       永无BUG

--- src/cn/ucai/fulicenter/view/FooterViewHolder.java
+++ src/cn/ucai/fulicenter/view/FooterViewHolder.java
@@ -7,8 +7,31 @@ import android.widget.TextView;
 import cn.ucai.fulicenter.R;
 
 /**
- * Created by clawpo on 16/4/16.
+ * Created by leon on 16/6/21.
+ *
  */
+/*
+                   _ooOoo_
+                  o8888888o
+                  88" . "88
+                  (| -_- |)
+                  O\  =  /O
+               ____/`---'\____
+             .'  \\|     |//  `.
+            /  \\|||  :  |||//  \
+           /  _||||| -:- |||||-  \
+           |   | \\\  -  /// |   |
+           | \_|  ''\---/''  |   |
+           \  .-\__  `-`  ___/-. /
+         ___`. .'  /--.--\  `. . __
+      ."" '<  `.___\_<|>_/___.'  >'"".
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+     \  \ `-.   \_ __\ /__ _/   .-` /  /
+======`-.____`-.___\_____/___.-`____.-'======
+                   `=---='
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+         佛祖保佑       永无BUG
+*/
 public class FooterViewHolder extends RecyclerView.ViewHolder {
     public TextView tvFooter;
     public FooterViewHolder(View itemView) {
