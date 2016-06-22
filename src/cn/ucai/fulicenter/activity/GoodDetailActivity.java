af03639ab433d92c377e3a5a90dd8e7d7ebcb0e0
leonFighting <379029170@qq.com>
2016-06-23 01:33:01 +0800

    集成三方分享功能

--- src/cn/ucai/fulicenter/activity/GoodDetailActivity.java
+++ src/cn/ucai/fulicenter/activity/GoodDetailActivity.java
@@ -19,6 +19,8 @@ import com.android.volley.toolbox.NetworkImageView;
 
 import java.util.ArrayList;
 
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
 import cn.ucai.fulicenter.D;
 import cn.ucai.fulicenter.FuLiCenterApplication;
 import cn.ucai.fulicenter.I;
@@ -81,13 +83,19 @@ public class GoodDetailActivity extends BaseActivity {
         setCollectClickListener();
         setCartClickListener();
         registerCartChangedReceiver();
+        mivShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showShare();
+            }
+        });
     }
 
     private void setCartClickListener() {
         mivAddCart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Utils.addCart(mContext,mGoodDetails);
+                Utils.addCart(mContext, mGoodDetails);
             }
         });
     }
@@ -139,18 +147,18 @@ public class GoodDetailActivity extends BaseActivity {
         return new Response.Listener<MessageBean>() {
             @Override
             public void onResponse(MessageBean messageBean) {
-                if(messageBean.isSuccess()){
-                    if(actionCollect == I.ACTION_ADD_COLLECT){
+                if (messageBean.isSuccess()) {
+                    if (actionCollect == I.ACTION_ADD_COLLECT) {
                         isCollect = true;
                         mivCollect.setImageResource(R.drawable.bg_collect_out);
                     }
-                    if(action == I.ACTION_DELETE_COLLECT){
+                    if (action == I.ACTION_DELETE_COLLECT) {
                         isCollect = false;
                         mivCollect.setImageResource(R.drawable.bg_collect_in);
                     }
                     new DownloadCollectCountTask(mContext).execute();
                 }
-                Utils.showToast(mContext,messageBean.getMsg(),Toast.LENGTH_SHORT);
+                Utils.showToast(mContext, messageBean.getMsg(), Toast.LENGTH_SHORT);
             }
         };
     }
@@ -255,11 +263,11 @@ public class GoodDetailActivity extends BaseActivity {
 
     private void initCartCount() {
         ArrayList<CartBean> cartList = FuLiCenterApplication.getInstance().getCartList();
-        if(cartList!=null&&cartList.size()>0){
+        if (cartList != null && cartList.size() > 0) {
             mtvCartCount.setVisibility(View.VISIBLE);
-            int count=Utils.sumCartCount();
-            Log.e(TAG,"CartChangedReceiver.count="+count);
-            mtvCartCount.setText(""+count);
+            int count = Utils.sumCartCount();
+            Log.e(TAG, "CartChangedReceiver.count=" + count);
+            mtvCartCount.setText("" + count);
         }
 //        if (count > 0) {
 //            mtvCartCount.setVisibility(View.VISIBLE);
@@ -308,6 +316,7 @@ public class GoodDetailActivity extends BaseActivity {
     }
 
     CartChangedReceiver mCartChangedReceiver;
+
     class CartChangedReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -316,16 +325,45 @@ public class GoodDetailActivity extends BaseActivity {
     }
 
     private void registerCartChangedReceiver() {
-        mCartChangedReceiver=new CartChangedReceiver();
-        IntentFilter filter=new IntentFilter("update_cart");
+        mCartChangedReceiver = new CartChangedReceiver();
+        IntentFilter filter = new IntentFilter("update_cart");
         registerReceiver(mCartChangedReceiver, filter);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(mCartChangedReceiver!=null){
+        if (mCartChangedReceiver != null) {
             unregisterReceiver(mCartChangedReceiver);
         }
+    }
+
+    private void showShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+
+// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+        //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+        oks.setTitle(getString(R.string.share));
+        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+
+// 启动分享GUI
+        oks.show(this);
     }
 }
